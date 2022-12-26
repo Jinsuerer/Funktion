13ae0f082f895b8f1c95b0519ceb911f0940ed6c
jin <hevi.viyan3@gmail.com>
2022-12-26 21:48:18 +0100

    first commit

---
+++ app.js
@@ -0,0 +1,23 @@
+// const a = +(prompt ("enter frist number"))
+// const b = +(prompt ("enter frist number"))
+
+// let sum = () => a + b ;
+// let sum2 = () => 25*(2*a + b)
+
+// console.log(sum());
+// console.log(sum2());
+
+// bir dairenin alanini bulmak
+
+// const daireninAlani = (r) => Math.PI*r*r
+// console.log(daireninAlani(6).toPrecision(4));
+// // .toPrecision basamagi ayarlar
+
+// const calculate = (year) =>new Date().getFullYear() - year
+// console.log(calculate (1996));
+
+
+
+// Vücud Kütle Indeksi
+
+const bmi = (weight, height) => weight / (height*height)
